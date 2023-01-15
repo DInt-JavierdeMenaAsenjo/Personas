89a694c325cdf1b82eee9bf9200c77fc16eae7f5
Javier de Mena Asenjo <javier.demena@iesdoctorbalmis.com>
2023-01-15 20:27:34 +0100

    Agregar archivos de proyecto.

---
+++ Personas/Servicios/ServicioNavegacion.cs
@@ -0,0 +1,22 @@
+﻿using Personas.Vistas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Personas
+{
+    class ServicioNavegacion
+    {
+        public UserControl CargaLista()
+        {
+            return new VentanaLista();
+        }
+        public UserControl CargaAñadir()
+        {
+            return new VentanaAñadir();
+        }
+    }
+}
---
+++ Personas/Vistas/MainWindow/MainWindowVM.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Personas
+{
+    class MainWindowVM : ObservableObject
+    {
+        private ServicioNavegacion sn;
+
+        public RelayCommand NuevaCommand { get; }
+        public RelayCommand ListaCommand { get; }
+
+        private UserControl contenidoMostrar;
+
+        public UserControl ContenidoMostrar
+        {
+            get { return contenidoMostrar; }
+            set { SetProperty(ref contenidoMostrar, value); }
+        }
+
+        public MainWindowVM()
+        {
+            NuevaCommand = new RelayCommand(CargarNueva);
+        }
+
+        public void CargarNueva()
+        {
+            ContenidoMostrar = sn.CargaAñadir();
+        }
+
+        public void CargarLista()
+        {
+            ContenidoMostrar = sn.CargaLista();
+        }
+    }
+}
